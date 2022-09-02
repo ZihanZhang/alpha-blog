64c53577d32bde1b5bfe2ebd37051e14d312792d
ZihanZhang <zihanzhang422@gmail.com>
2022-09-01 17:37:57 -0700

    pagination

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,29 @@
 class UsersController < ApplicationController
+
+  def show
+    @user = User.find(params[:id])
+    @articles = @user.articles.paginate(page: params[:page], per_page: 2)
+  end
   def new
     @user = User.new
+  end
+
+  def index
+    @users = User.paginate(page: params[:page], per_page: 2)
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:notice] = "Your account info has been updated"
+      redirect_to @user
+    else
+      render 'edit'
+    end
   end
   
   def create
